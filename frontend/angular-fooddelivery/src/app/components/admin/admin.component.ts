9dbc9eea89a47d3d509582a04c504040080b438c
LiontoDionisis <DionisisLiontos@outlook.com>
2024-06-18 18:39:48 +0300

    frontend

--- frontend/angular-fooddelivery/src/app/components/admin/admin.component.ts
+++ frontend/angular-fooddelivery/src/app/components/admin/admin.component.ts
@@ -3,6 +3,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/login-service.service';
+import { FoodService } from '../../services/food.service';
+
+interface FoodItem {
+  id: number;
+  name: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-admin',
@@ -11,12 +18,17 @@ import { AuthService } from '../../services/login-service.service';
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
   providers: [
-    AuthService
+    AuthService,
+    FoodService
   ]
 })
 export class AdminComponent {
+  foodItems: FoodItem[] = [];
 
-  constructor(private authService: AuthService, private router: Router) { }
+  isNavbarCollapsed = true;
+
+
+  constructor(private authService: AuthService, private router: Router, private foodService: FoodService) { }
 
   ngOnInit(): void {
     const userRole = this.authService.getUserRole();
@@ -26,5 +38,45 @@ export class AdminComponent {
     if (userRole !== "admin" && this.authService.isLoggedIn()) {
       this.router.navigate(['/home']);
     }
+    this.loadFoodItems();
+  }
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  closeNavbar() {
+    this.isNavbarCollapsed = true;
+  }
+
+  logout(): void {
+    if (window.confirm("Are you sure you want to logout?")) {
+      this.authService.logout();
+    }
+  }
+
+  isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
+
+  private loadFoodItems() {
+    this.foodService.getFoodItems().subscribe(
+      (response: any) => {
+        if (response && response['$values']) {
+          // Extracting food items from the $values array
+          this.foodItems = response['$values'].map((item: any) => {
+            return {
+              id: item.id,
+              name: item.name,
+              price: item.price
+            };
+          });
+        }
+      },
+      error => {
+        console.error('Error fetching food items:', error);
+      }
+    );
+
   }
 }
